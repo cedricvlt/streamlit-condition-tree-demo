2f7f8cc2a59a6f03bbc2e2dd22aa33bb19a875f5
cedricvlt <cedric_villette@hotmail.fr>
2023-09-30 19:48:04 +0200

    v0.1.2

---
+++ 1_DataFrame_filter.py
@@ -0,0 +1,223 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+from streamlit_condition_tree import condition_tree, config_from_dataframe
+
+st.set_page_config(
+    page_title='Streamlit-condition-tree demo',
+    page_icon=':beer:'
+)
+
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv(
+        'https://media.githubusercontent.com/media/datablist/sample-csv-files/main/files/people/people-100.csv',
+        index_col=0,
+        parse_dates=['Date of birth'],
+        date_format='%Y-%m-%d')
+    df['Age'] = ((pd.Timestamp.today() - df['Date of birth']).dt.days / 365).astype(int)
+    df['Sex'] = pd.Categorical(df['Sex'])
+    df['Likes tomatoes'] = np.random.randint(2, size=df.shape[0]).astype(bool)
+    return df
+
+
+st.subheader('Initial DataFrame')
+df = load_data()
+st.dataframe(df)
+
+config = config_from_dataframe(df)
+
+tree = {
+    "type": "group",
+    "properties": {
+        "conjunction": "OR"
+    },
+    "children": [
+        {
+            "type": "group",
+            "properties": {
+                "conjunction": "AND",
+                "not": False
+            },
+            "children": [
+                {
+                    "type": "rule",
+                    "properties": {
+                        "fieldSrc": "field",
+                        "field": "Age",
+                        "operator": "between",
+                        "value": [
+                            15,
+                            70
+                        ],
+                        "valueSrc": [
+                            "value",
+                            'null'
+                        ],
+                        "valueType": [
+                            "number",
+                            "number"
+                        ]
+                    }
+                },
+                {
+                    "type": "rule",
+                    "properties": {
+                        "fieldSrc": "field",
+                        "field": "`First Name`",
+                        "operator": "like",
+                        "value": [
+                            "M"
+                        ],
+                        "valueSrc": [
+                            "value"
+                        ],
+                        "valueType": [
+                            "text"
+                        ]
+                    }
+                },
+                {
+                    "type": "rule",
+                    "properties": {
+                        "fieldSrc": "field",
+                        "field": "`Last Name`",
+                        "operator": "not_equal",
+                        "value": [
+                            "`First Name`"
+                        ],
+                        "valueSrc": [
+                            "field"
+                        ],
+                        "valueType": [
+                            "text"
+                        ]
+                    }
+                },
+                {
+                    "type": "rule",
+                    "properties": {
+                        "fieldSrc": "field",
+                        "field": "`Likes tomatoes`",
+                        "operator": "equal",
+                        "value": [
+                            True
+                        ],
+                        "valueSrc": [
+                            "value"
+                        ],
+                        "valueType": [
+                            "boolean"
+                        ]
+                    }
+                }
+            ]
+        },
+        {
+            "type": "group",
+            "properties": {
+                "conjunction": "AND",
+                "not": False
+            },
+            "children": [
+                {
+                    "type": "rule",
+                    "properties": {
+                        "fieldSrc": "field",
+                        "field": "`First Name`",
+                        "operator": "not_like",
+                        "value": [
+                            "Goerges"
+                        ],
+                        "valueSrc": [
+                            "value"
+                        ],
+                        "valueType": [
+                            "text"
+                        ]
+                    }
+                },
+                {
+                    "type": "rule",
+                    "properties": {
+                        "fieldSrc": "field",
+                        "field": "Sex",
+                        "operator": "select_any_in",
+                        "value": [
+                            [
+                                "Female",
+                                "Male"
+                            ]
+                        ],
+                        "valueSrc": [
+                            "value"
+                        ],
+                        "valueType": [
+                            "multiselect"
+                        ]
+                    }
+                },
+                {
+                    "type": "rule",
+                    "properties": {
+                        "fieldSrc": "field",
+                        "field": "`Date of birth`",
+                        "operator": "between",
+                        "value": [
+                            "2023-09-19 22:26:16",
+                            "2023-09-28 23:26:20"
+                        ],
+                        "valueSrc": [
+                            "value",
+                            "value"
+                        ],
+                        "valueType": [
+                            "datetime",
+                            "datetime"
+                        ]
+                    }
+                }
+            ]
+        }
+    ]
+}
+
+st.subheader('Condition tree')
+query_string = condition_tree(
+    config,
+    tree=tree
+)
+st.code(query_string)
+st.markdown('')
+
+st.subheader('Filtered DataFrame')
+df = df.query(query_string)
+st.dataframe(df)
+
+with st.expander('Code'):
+    st.code("""
+    df = pd.read_csv(
+        'https://media.githubusercontent.com/media/datablist/sample-csv-files/main/files/people/people-100.csv',
+        index_col=0,
+        parse_dates=['Date of birth'],
+        date_format='%Y-%m-%d')
+    df['Age'] = ((pd.Timestamp.today() - df['Date of birth']).dt.days / 365).astype(int)
+    df['Sex'] = pd.Categorical(df['Sex'])
+    df['Likes tomatoes'] = np.random.randint(2, size=df.shape[0]).astype(bool)
+    
+    st.subheader('Initial DataFrame')
+    st.dataframe(df)
+    
+    st.subheader('Condition tree')
+    
+    config = config_from_dataframe(df)
+    query_string = condition_tree(config)
+    
+    st.code(query_string)
+    
+    st.subheader('Filtered DataFrame')
+    df = df.query(query_string)
+    st.dataframe(df)
+    
+    """, 'python')
---
+++ pages/2_Query build.py
@@ -0,0 +1,222 @@
+import streamlit as st
+from streamlit_condition_tree import condition_tree
+
+
+st.set_page_config(
+  page_title='Streamlit-condition-tree demo',
+  page_icon=':beer:'
+)
+
+config = {
+    'fields': {
+        'firstName': {
+            'label': 'First name',
+            'type': 'text',
+            'mainWidgetProps': {
+                'valuePlaceholder': 'Enter name',
+            },
+        },
+        'age': {
+            'label': 'Age',
+            'type': 'number',
+            'fieldSettings': {
+                'min': 0,
+                'max': 140
+            },
+            'preferWidgets': ['slider', 'rangeslider'],
+        },
+        'color': {
+            'label': 'Favorite color',
+            'type': 'select',
+            'fieldSettings': {
+                'listValues': [
+                    {'value': 'yellow', 'title': 'Yellow'},
+                    {'value': 'green', 'title': 'Green'},
+                    {'value': 'orange', 'title': 'Orange'},
+                ],
+            },
+        },
+        'like_tomatoes': {
+            'label': 'Likes tomatoes',
+            'type': 'boolean',
+            'operators': ['equal'],
+        },
+        'birth_date': {
+            'label': 'Date of birth',
+            'type': 'date',
+            'operators': ['less', 'equal']
+        }
+    },
+}
+
+tree = {
+    "type": "group",
+    "children": [
+        {
+            "type": "rule",
+            "properties": {
+                "fieldSrc": "field",
+                "field": "firstName",
+                "operator": "equal",
+                "value": [
+                    "Georges"
+                ],
+                "valueSrc": [
+                    "value"
+                ],
+                "valueType": [
+                    "text"
+                ]
+            }
+        },
+        {
+            "type": "rule",
+            "properties": {
+                "fieldSrc": "field",
+                "field": "age",
+                "operator": "less_or_equal",
+                "value": [
+                    75
+                ],
+                "valueSrc": [
+                    "value"
+                ],
+                "valueType": [
+                    "number"
+                ]
+            }
+        },
+        {
+            "type": "rule",
+            "properties": {
+                "fieldSrc": "field",
+                "field": "like_tomatoes",
+                "operator": "equal",
+                "value": [
+                    True
+                ],
+                "valueSrc": [
+                    "value"
+                ],
+                "valueType": [
+                    "boolean"
+                ]
+            }
+        },
+        {
+            "type": "rule",
+            "properties": {
+                "fieldSrc": "field",
+                "field": "birth_date",
+                "operator": "less",
+                "value": [
+                    "2023-09-18"
+                ],
+                "valueSrc": [
+                    "value"
+                ],
+                "valueType": [
+                    "date"
+                ]
+            }
+        },
+        {
+            "type": "rule",
+            "properties": {
+                "fieldSrc": "field",
+                "field": "color",
+                "operator": "select_equals",
+                "value": [
+                    "green"
+                ],
+                "valueSrc": [
+                    "value"
+                ],
+                "valueType": [
+                    "select"
+                ]
+            }
+        }
+    ]
+}
+
+return_type = st.selectbox(
+    'Return type',
+    ['queryString', 'mongodb', 'sql',
+     'spel', 'elasticSearch', 'jsonLogic'],
+    index=2
+)
+
+return_val = condition_tree(
+    config,
+    tree=tree,
+    return_type=return_type,
+    placeholder='Empty tree'
+)
+
+with st.expander('Return value', expanded=True):
+    if isinstance(return_val, dict):
+        st.write(return_val)
+    else:
+        st.code(return_val)
+
+with st.expander('Code'):
+    code = f"""
+
+    from streamlit_condition_tree import condition_tree
+
+
+    config = {{
+      'fields': {{
+          'firstName': {{
+            'label': 'First name',
+            'type': 'text',
+            'mainWidgetProps': {{
+              'valuePlaceholder': 'Enter name',
+            }},
+          }},
+          'age': {{
+            'label': 'Age',
+            'type': 'number',
+            'fieldSettings': {{
+              'min': 0,
+              'max': 140
+            }},
+            'preferWidgets': ['slider', 'rangeslider'],
+          }},
+          'color': {{
+            'label': 'Favorite color',
+            'type': 'select',
+            'fieldSettings': {{
+              'listValues': [
+                  {{'value': 'yellow', 'title': 'Yellow'}},
+                  {{'value': 'green', 'title': 'Green'}},
+                  {{'value': 'orange', 'title': 'Orange'}},
+              ],
+            }},
+          }},
+          'like_tomatoes': {{
+            'label': 'Likes tomatoes',
+            'type': 'boolean',
+            'operators': ['equal'],
+          }},
+          'birth_date': {{
+            'label': 'Date of birth',
+            'type': 'date',
+            'operators': ['less', 'equal']
+          }}
+      }},
+    }}
+
+    condition_tree(
+      config,
+      return_type='{return_type}',
+      placeholder='Empty tree'
+    )
+
+    # This shows the generated code for the query
+    st.write(condition_tree)
+
+        """
+
+    st.code(code, 'python')
